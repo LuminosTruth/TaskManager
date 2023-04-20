0762f1ca2c9907bd32428278d966078ff6149930
KiraCrypt <119428680+LuminosTruth@users.noreply.github.com>
2023-04-20 05:11:39 +0300

    Добавил токены, авторизацию и.т.д.

--- models/models.py
+++ models/models.py
@@ -1,25 +1,27 @@
-import sqlalchemy
-from pydantic.datetime_parse import datetime
-from sqlalchemy import MetaData, Integer, String, TIMESTAMP, JSON, ForeignKey, Table, Column
+from datetime import datetime
+
+from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON, Boolean
 
 metadata = MetaData()
 
-roles = Table(
-    "user_roles",
+role = Table(
+    "role",
     metadata,
     Column("id", Integer, primary_key=True),
-    Column("role_name", String, nullable=False),
-    Column("role_permissions", JSON)
+    Column("name", String, nullable=False),
+    Column("permissions", JSON),
 )
 
-users = Table(
-    "users",
+user = Table(
+    "user",
     metadata,
     Column("id", Integer, primary_key=True),
     Column("email", String, nullable=False),
     Column("username", String, nullable=False),
-    Column("password", String, nullable=False),
     Column("registered_at", TIMESTAMP, default=datetime.utcnow),
-    Column("role_id", Integer, ForeignKey("user_roles.id"))
+    Column("role_id", Integer, ForeignKey(role.c.id)),
+    Column("hashed_password", String, nullable=False),
+    Column("is_active", Boolean, default=True, nullable=False),
+    Column("is_superuser", Boolean, default=False, nullable=False),
+    Column("is_verified", Boolean, default=False, nullable=False),
 )
-
