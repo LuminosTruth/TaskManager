1bc5657850e7021d7bb8634b0a0a4324494eeda4
KiraCrypt <119428680+LuminosTruth@users.noreply.github.com>
2023-04-21 17:22:04 +0300

    Добавил Celery + Redis

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from redis import asyncio as aioredis
 
 from src.auth.base_config import auth_backend, fastapi_users
-from src.auth.schemas import UserRead, UserCreate
-
+from src.auth.schemas import UserCreate, UserRead
 from src.operations.router import router as router_operation
+from src.tasks.router import router as router_tasks
 
 app = FastAPI(
-    title="TaskManager App"
+    title="Trading App"
 )
 
 app.include_router(
@@ -22,3 +25,10 @@ app.include_router(
 )
 
 app.include_router(router_operation)
+app.include_router(router_tasks)
+
+
+@app.on_event("startup")
+async def startup_event():
+    redis = aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
--- src/operations/router.py
+++ src/operations/router.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter, Depends
-from sqlalchemy import select, insert
+import time
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi_cache.decorator import cache
+from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database import get_async_session
@@ -12,15 +15,37 @@ router = APIRouter(
 )
 
 
-@router.get("/")
+@router.get("/long_operation")
+@cache(expire=30)
+def get_long_op():
+    time.sleep(2)
+    return "Много много данных, которые вычислялись сто лет"
+
+
+@router.get("")
 async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
-    query = select(operation).where(operation.c.type == operation_type)
-    result = await session.execute(query)
-    return result.all()
+    try:
+        query = select(operation).where(operation.c.type == operation_type)
+        result = await session.execute(query)
+        return {
+            "status": "success",
+            "data": result.all(),
+            "details": None
+        }
+    except Exception:
+        # Передать ошибку разработчикам
+        raise HTTPException(status_code=500, detail={
+            "status": "error",
+            "data": None,
+            "details": None
+        })
 
 
-@router.post("/")
+@router.post("")
 async def add_specific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
+    """
+    docstring
+    """
     stmt = insert(operation).values(**new_operation.dict())
     await session.execute(stmt)
     await session.commit()
